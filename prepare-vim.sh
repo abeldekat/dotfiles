61f2a7ff0db05bab21f62f32b5e34ed046da7c16
chris <chris.mousset@gmail.com>
2019-12-01 20:41:33 +0100

    Other approach for minpac

--- prepare-vim.sh
+++ prepare-vim.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
-set DOTFILES=~/dotfiles
+DOTFILES=~/dotfiles
+MINPACPATH=$DOTFILES/vim/pack/minpac/opt
 
 mkdir -p ~/.config/nvim
 mkdir -p ~/tmp
 
 echo "Apply ln"
-ln -s $DOTFILES/vim ~/.vim
-ln -s $DOTFILES/init.vim ~/.config/nvim/init.vim
+ln -s -f $DOTFILES/vim ~/.vim
+ln -s -f $DOTFILES/init.vim ~/.config/nvim/init.vim
 
 echo "Apply ln git: Reminder for  .gitconfig.local"
-ln -s $DOTFILES/global-gitignore ~/.gitignore
-ln -s $DOTFILES/gitconfig ~/.gitconfig
+ln -s -f $DOTFILES/global-gitignore ~/.gitignore
+ln -s -f $DOTFILES/gitconfig ~/.gitconfig
+
+echo "Get minpac"
+git clone https://github.com/k-takata/minpac.git $MINPACPATH
+
+echo "Done"
