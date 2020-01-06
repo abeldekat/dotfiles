88292496e1ba889482866b80189b97c16da84b6a
abledekat <abeldekatt@gmail.com>
2020-01-06 15:52:41 +0100

    Aliases

--- zsh/.config/oh-my-zsh/custom/aliases.zsh
+++ zsh/.config/oh-my-zsh/custom/aliases.zsh
@@ -1,12 +1,12 @@
-alias config='/usr/bin/git --git-dir=$HOME/.config/dotfiles/ --work-tree=$HOME'
-#config config --local status.showUntrackedFiles no
+alias ezsh="vim ~/.config/zsh/.zshrc"
+alias eozsh="vim $ZSH/.oh-my-zsh.sh"
+alias ev="vim ~/.vim/vimrc"
+alias ealiases="vim $ZSH/custom/aliases.zsh"
 
-alias eezsh="vi ~/.zshrc"
-alias eeozsh="vi $ZSH/.oh-my-zsh.sh"
-alias eevi="vi ~/.vim/vimrc"
+alias szsh="source $HOME/config/zsh/.zshrc"
 
-alias sszsh="source $HOME/.zshrc"
 alias clr="clear"
 
+alias vi="vim"
 alias fd="fdfind"
 alias dirs="dirs -v"
