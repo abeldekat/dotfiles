458767396ecfad2262341787ee22e62d0b567cf5
abledekat <abeldekatt@gmail.com>
2020-01-08 23:03:45 +0100

    Aliases startx

--- zsh/.config/oh-my-zsh/custom/aliases.zsh
+++ zsh/.config/oh-my-zsh/custom/aliases.zsh
@@ -4,13 +4,22 @@ alias ev="vim ~/.vim/vimrc"
 alias ealiases="vim $ZSH/custom/aliases.zsh"
 alias eozsh="vim $ZSH/.oh-my-zsh.sh"
 
+# dotfiles
 alias ereadme="vim ~/dotfiles/README.md"
 alias cdd="cd ~/dotfiles"
 
 alias szsh="source $CFGP/zsh/.zshrc"
 
 # arch git alias does not work. Make a link from vim to vi in /usr/bin
+# Command aliases
 alias fd="fdfind"
 alias clr="clear"
 alias dirs="dirs -v"
+
+# ZSH guide pdf copy helper
+alias %=' '
+
+# Startx
+alias sxb='startx ~/.xinitrc'
+alias sxx='startx ~/.xinitrc xfce'
 
