d09dbebff9c57326d8835b65d4bfa88afbaa0549
abel <abel@nomail.com>
2020-01-19 16:24:59 +0100

    Xorg specific

--- Archinstall-nostow/desktopspecific.sh
+++ Archinstall-nostow/desktopspecific.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-pacman -S xf86-video-ati
+# xorg installs mesa, driver for 3d acceleration
+# mesa-vdpau, video acceleration
+# xf86-video-ati ddx driver 2d acc in xorg
+# desktop card: Radeon HD 5750
+pacman -S xorg-server xorg-xwininfo xorg-init mesa-vdpau xf86-video-ati xdo
--- Archinstall-nostow/laptopspecific.sh
+++ Archinstall-nostow/laptopspecific.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Laptop only lspci | grep -e VGA -e 3D
-pacman -S xf86-video-intel xf86-input-libinput
+pacman -S xorg-server xorg-xwininfo xorg-init xf86-video-intel xf86-input-libinput xdo
