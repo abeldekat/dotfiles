7c17328ab948e6e1cde7fbf8eb9049e17f386a6b
abel <abel@nomail.com>
2020-01-19 11:30:11 +0100

    Correctie git conf arch install

--- scripts/.local/bin/dots/dotsinstall-prepare.sh
+++ scripts/.local/bin/dots/dotsinstall-prepare.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+# Script for easy stow on a clean arch system
+
 DOTFILES=$HOME/dotfiles
 
-# If stow discovers an existing .vim directory symlinking will be diffent
-# .vim should be a link to .vim in the dotfiles...
+# Add minpac
 PACKPATH=$DOTFILES/vim/.vim/pack
 if [ ! -d $PACKPATH ] ; then
 	echo "Adding minpack to the dotfiles folder"
         git clone https://github.com/k-takata/minpac.git $PACKPATH/minpac/opt/minpac
 fi
+
+# .vim must be a link to .vim in the dotfiles...
 if [ ! -L $HOME/.vim ] && [ -d $HOME/.vim ] ; then
   echo "Renaming existing .vim dir..."
   mv $HOME/.vim $HOME/.vim-MARKEDBYABEL
@@ -16,6 +19,16 @@ fi
 if [ -f $HOME/.vimrc ] ; then
   echo "Renaming existing .vimrc ..."
   mv $HOME/.vimrc $HOME/.vimrc-MARKEDBYABEL
+fi
+
+# On a clean system bash will be present
+if [ -f $HOME/.bashrc ] ; then
+  echo "Renaming existing .bashrc ..."
+  mv $HOME/.bashrc $HOME/.bashrc-MARKEDBYABEL
+fi
+if [ -f $HOME/.bash_profile ] ; then
+  echo "Renaming existing .bash_profile ..."
+  mv $HOME/.bash_profile $HOME/.bash_profile-MARKEDBYABEL
 fi
 
 # Test stow
