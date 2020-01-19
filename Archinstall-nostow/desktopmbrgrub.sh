0cda188489c2d83c400893459997a5a6bb6fbeae
abel <abel@nomail.com>
2020-01-19 10:34:53 +0100

    Correctie git conf arch install

--- Archinstall-nostow/desktopmbrgrub.sh
+++ Archinstall-nostow/desktopmbrgrub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Master boot record specific instructions
-pacman -S grub osprober
+pacman -S grub os-prober
 grub-install --target=i386-pc /dev/sdc
 grub-mkconfig -o /boot/grub/grub.cfg
