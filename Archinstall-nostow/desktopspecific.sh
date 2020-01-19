439c47c108211cb606868201ea03447b79e0c9a8
abel <abel@nomail.com>
2020-01-19 21:39:25 +0100

    Added needed aur packages. Refactored the installation order and the pkglist

--- Archinstall-nostow/desktopspecific.sh
+++ Archinstall-nostow/desktopspecific.sh
@@ -3,4 +3,4 @@
 # mesa-vdpau, video acceleration
 # xf86-video-ati ddx driver 2d acc in xorg
 # desktop card: Radeon HD 5750
-pacman -S xorg-server xorg-apps xorg-init mesa-vdpau xf86-video-ati xdo
+pacman -S mesa-vdpau xf86-video-ati
--- Archinstall-nostow/mirrors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Modifying mirrorlist to the Netherlands"
-cd /etc/pacman.d
-cp mirrorlist orgmirrorlist
-grep -A 1 "Netherlands" orgmirrolist | sed /--/d > mirrorlist
