32dd498ba6995660f4dfb20e0c105d789b666f8c
abel <abel@nomail.com>
2020-01-19 10:35:36 +0100

    Correctie git conf arch install

--- Archinstall-nostow/laptopefigrub.sh
+++ Archinstall-nostow/laptopefigrub.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-pacman -S grub efibootmgr osprober
+pacman -S grub efibootmgr os-prober
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
