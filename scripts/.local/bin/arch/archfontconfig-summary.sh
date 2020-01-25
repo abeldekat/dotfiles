1068cc5c4d5740207e16cf6ca86f711811409c68
abel <abel@nomail.com>
2020-01-25 11:35:10 +0100

    Fonts. Added fontconfig..

---
+++ scripts/.local/bin/arch/archfontconfig-summary.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Library, fonts to applications. Freetype2 font rendering packages, bytecode interpreter BCI.
+
+# Though Fontconfig is used often in modern Unix and Unix-like operating systems, some applications rely on the original method of font selection and display, the X Logical Font Description.
+# /usr/share/fonts ~/.local/share/fonts
+
+# fc-list: file
+# xorg known font paths
+grep /fonts ~/.local/share/xorg/Xorg.0.log
+xset q
+# fonts known to xorg. Change with xset +fp
+xlsfonts
+
+# conf.d and conf.avail, linking enables...Also possible in xdg home
+# hintfull for fonts is not necessary for displays that have around 300dpi
+xdpyinfo | grep dots
+
+# To find out what settings are in effect, use fc-match --verbose. eg. 
+fc-match --verbose Sans
