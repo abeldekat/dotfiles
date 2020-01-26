3fa9c91ed96291032d5966656bea6125619d23a0
abel <abel@nomail.com>
2020-01-26 18:49:47 +0100

    enabled touchpad tap. Also the fp hehash is not necessary

--- Archinstall-nostow/laptopspecific.sh
+++ Archinstall-nostow/laptopspecific.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Laptop only lspci | grep -e VGA -e 3D
 pacman -S xf86-video-intel xf86-input-libinput
+scriptdir=`dirname $0`
+sudo cp $scriptdir/30-touchpad-laptop.conf /etc/X11/xorg.conf.d/
