d878d760f1727db7ad4b14f48e97e397e9ecea43
abel <abel@nomail.com>
2020-01-27 19:08:30 +0100

    termite base16 themes...

--- zsh/.config/zsh/zsh_framework.zsh
+++ zsh/.config/zsh/zsh_framework.zsh
@@ -21,7 +21,7 @@ source $ZGEN_DIR/zgen.zsh
 if ! zgen saved; then
 	zgen oh-my-zsh
 	zgen oh-my-zsh plugins/git
-	zgen oh-my-zsh plugins/colored-man-pages
+	#zgen oh-my-zsh plugins/colored-man-pages
 	zgen oh-my-zsh plugins/vi-mode
 	zgen oh-my-zsh themes/robbyrussell
 
@@ -31,7 +31,7 @@ if ! zgen saved; then
 	zgen load /usr/share/fzf/key-bindings.zsh
 	zgen load /usr/share/fzf/completion.zsh
 
-	zgen load zsh-users/zsh-syntax-highlighting 
+	#zgen load zsh-users/zsh-syntax-highlighting 
 	zgen load zsh-users/zsh-history-substring-search 
 	zgen save
 fi
