adab9388028ef66076c203cc7234620f9f9946c2
abel <abel@nomail.com>
2020-02-15 13:52:00 +0100

    Reinstall desktop. Provide with default theme on clean install

--- scripts/.local/bin/arch/pkglist-git.sh
+++ scripts/.local/bin/arch/pkglist-git.sh
@@ -4,6 +4,5 @@ TARGET="$HOME"/.local/share
 # tempus theme
 if [ ! -d "$TARGET/tempus-themes" ] ; then
 	git clone https://gitlab.com/protesilaos/tempus-themes.git "$TARGET/tempus-themes"
-	mkdir "$MY_COLORS"
 	ln -s "$TARGET/tempus-themes/xcolors/" "$MY_COLORS/xcolors"
 fi
