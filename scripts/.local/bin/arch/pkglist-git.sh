ed17067ad6c709a187884fd6f8536a385cb70f08
abel <abel@nomail.com>
2020-02-16 21:36:32 +0100

    Laptop blocks in subdirectoy

--- scripts/.local/bin/arch/pkglist-git.sh
+++ scripts/.local/bin/arch/pkglist-git.sh
@@ -6,3 +6,4 @@ if [ ! -d "$TARGET/tempus-themes" ] ; then
 	git clone https://gitlab.com/protesilaos/tempus-themes.git "$TARGET/tempus-themes"
 	ln -s "$TARGET/tempus-themes/xcolors/" "$MY_COLORS/xcolors"
 fi
+#git clone https://github.com/andreafabrizi/Dropbox-Uploader.git
