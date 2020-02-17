e6c357cb6692b93babca821e3444463abf6edef8
abeldekat <abeldekat@nomail.com>
2020-02-17 21:31:44 +0100

    Dotsinstall prepare: Vim now takes care of minpac plugin

--- scripts/.local/bin/dots/dotsinstall-prepare.sh
+++ scripts/.local/bin/dots/dotsinstall-prepare.sh
@@ -7,23 +7,6 @@ MY_DEFAULTS="$DOTFILES/Archinstall-nostow/defaults"
 MY_COLORS="$HOME/.local/share/my_colors"
 MY_TERMITE="$HOME/.config/termite"
 
-# Add minpac
-PACKPATH="$DOTFILES/vim/.vim/pack"
-if [ ! -d $PACKPATH ] ; then
-	echo "Adding minpack to the dotfiles folder"
-        git clone https://github.com/k-takata/minpac.git "$PACKPATH/minpac/opt/minpac"
-fi
-
-# .vim must be a link to .vim in the dotfiles...
-if [ ! -L "$HOME/.vim" ] && [ -d "$HOME/.vim" ] ; then
-  echo "Renaming existing .vim dir..."
-  mv "$HOME/.vim" "$HOME/.vim-MARKEDBYABEL"
-fi
-if [ -f "$home/.vimrc" ] ; then
-  echo "Renaming existing .vimrc ..."
-  mv "$HOME/.vimrc" "$HOME/.vimrc-MARKEDBYABEL"
-fi
-
 if [ ! -d "$MY_COLORS" ] ; then
   echo "Adding $MY_COLORS ..."
   mkdir -p "$MY_COLORS"
