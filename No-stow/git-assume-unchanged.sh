d1e65455aa5ee7bca92fef0e768bafab85c0c3bc
abel <abel@nomail.com>
2020-02-20 19:33:15 +0100

    Added files to git assume-unchanged. Changed the way termite config is merged. It now preserves the link

--- No-stow/git-assume-unchanged.sh
+++ No-stow/git-assume-unchanged.sh
@@ -3,4 +3,8 @@
 # Some files are subject to generation. They are provided in this repo as initial defaults.  
 # Any changes will not be reported nor committed by git... 
 # Revert with --no-assume-unchanged
+# List with git ls-files -v | grep '^[[:lower:]]'
+
 git update-index --assume-unchanged ../termite/.config/termite/config 
+
+git update-index --assume-unchanged $(git ls-files ../base-colors-always-stow | tr '\n' ' ')
