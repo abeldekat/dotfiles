fe33bf1567e3adb0ff1b2199d839e48eba69d6d6
abel <abel@nomail.com>
2020-02-25 20:00:39 +0100

    WIP git packages integration

--- stow-always-base/.local/bin/arch/pkglist-git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TARGET="$HOME"/.local/share
-
-# tempus theme
-if [ ! -d "$TARGET/tempus-themes" ] ; then
-	git clone https://gitlab.com/protesilaos/tempus-themes.git "$TARGET/tempus-themes"
-	ln -s "$TARGET/tempus-themes/xcolors/" "$MY_COLORS/xcolors"
-fi
-#git clone https://github.com/andreafabrizi/Dropbox-Uploader.git
