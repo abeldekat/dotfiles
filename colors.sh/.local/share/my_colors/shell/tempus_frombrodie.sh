eacccd57aaf48f5b01cb4c03d89ea402f10e9f65
abel <abel@nomail.com>
2020-08-24 21:12:20 +0000

    Tempus vim: default to autumn when there is no vim theme present

--- colors.sh/.local/share/my_colors/shell/tempus_frombrodie.sh
+++ colors.sh/.local/share/my_colors/shell/tempus_frombrodie.sh
@@ -2,9 +2,16 @@
 # Copied from st config from Brodie Robertson
 
 # Special
-background='#191919'
-foreground='#d8dee9'
-cursor='#d8dee9'
+background='#191919' # unique color
+foreground='#d8dee9' # unique color
+cursor='#d8dee9' # unique color
+
+# -- cpdfd
+backgroundalt="#676f78" # tempus color8, also backgrounddim
+foregroundalt="#acadb1" # tempus color7
+cursorColor="#e0e3e7" # tempus color 15 and fg
+cursorColor2="#313539" # tempus color0
+# -- cpdfd
 
 # colors
 color0='#313539'
@@ -23,3 +30,9 @@ color12='#707fd0'
 color13='#c583d0'
 color14='#8adaf1'
 color15='#e0e3e7'
+
+
+# -- cpdfd
+backgrounddim="#676f78" # color8
+#backgrounddim="#313539" # test color0
+# -- cpdfd
