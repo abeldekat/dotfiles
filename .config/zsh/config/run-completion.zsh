d3c01bc8fb549f31c0e75ea8a145a8dec8e774d6
abel <abel@nomail.com>
2021-01-26 22:42:35 +0100

    Added alias to remove zcompdump

--- .config/zsh/config/run-completion.zsh
+++ .config/zsh/config/run-completion.zsh
@@ -1,6 +1,6 @@
 # -U means no alias expansion
 # autoload -U compaudit compinit
-#
+
 # run and avoid security tests
 # compinit -u
 # zsh compinit: insecure directories, run compaudit for list.
@@ -8,9 +8,10 @@
 # With zgen, init, compinit is done lastly
 # autoload -Uz compinit && \
 #    compinit -C 
-
 # OMZ first autoloads, then adds plugins to fpath etc, and runs the compinit before sourcing the plugins
 
 autoload -U compinit
 # OMZ: If the user wants it, load from all found directories
 compinit -u -C -d "$XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION"
+
+alias rmdump="rm $XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION"
