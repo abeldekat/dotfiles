b7307cb2c1a0aa34deeed8e1413d44c2b77d2605
abel <abel@nomail.com>
2021-01-31 16:10:48 +0100

    tmux man cannot get key binding to work

--- .config/zsh/config/tmux_man.zsh
+++ .config/zsh/config/tmux_man.zsh
@@ -1,15 +1,20 @@
 tmux-man-for-current-word() {
-  cmd=$(echo "$BUFFER" | rev | sed -E 's/^ +//' | cut -d ' ' -f 1 | rev)
-  width=$(tmux display -p '#{pane_width}')
-  height=$(tmux display -p '#{pane_height}')
-  normalized_height=$( echo "$height * 2.2" | bc )
+  # cmd=$(echo "$BUFFER" | rev | sed -E 's/^ +//' | cut -d ' ' -f 1 | rev)
+  # width=$(tmux display -p '#{pane_width}')
+  # height=$(tmux display -p '#{pane_height}')
+  # normalized_height=$( echo "$height * 2.2" | bc )
 
-  if (( normalized_height > width )); then
-    tmux split-window -v "man $cmd"
-  else
-    tmux split-window -h "man $cmd"
-  fi
+  # if (( normalized_height > width )); then
+  #   tmux split-window -v "man $cmd"
+  # else
+  #   tmux split-window -h "man $cmd"
+  # fi
+  tmux split-window -h
 }
+
 # TODO Does not work yet
+# OMZ man plugin only works if put before vi-mode
+# That uses '\e'man as bindkey
 zle -N tmux-man-for-current-word
 bindkey '^Q' tmux-man-for-current-word
+# bindkey "\e"man tmux-man-for-current-word
