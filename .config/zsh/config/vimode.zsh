bd4a5c756b6f167590944f11ccda6e3903a2961b
abel <abel@nomail.com>
2021-02-01 16:12:19 +0100

    Fix backspace bug zsh vimode

--- .config/zsh/config/vimode.zsh
+++ .config/zsh/config/vimode.zsh
@@ -8,7 +8,7 @@
 bindkey -v
 export KEYTIMEOUT=1
 
-# TODO This is done by FZF...Enable searching through history
+# This is done by FZF...Enable searching through history
 # Shows a bck-i-search, also tested with prompt:
 # bindkey '^R' history-incremental-pattern-search-backward
 
@@ -21,11 +21,11 @@ bindkey '^x^e' edit-command-line
 # man zshall example overwrites v in normal mode 
 # bindkey -M vicmd v edit-command-line
 
-# Fix backspace bug when switching modes
-# bindkey "^?" backward-delete-char
-
 # Use vim keys in tab complete menu:
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
+# Fix backspace bug when switching modes
+# pressing a: can't backspace past the point where I entered insert mode.
+bindkey "^?" backward-delete-char
